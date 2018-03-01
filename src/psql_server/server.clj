75bbbc894e1d1ae66d36afc96d521ab9ae02e5ba
Adam <adam.cumiskey@gmail.com>
2018-03-01 15:21:24 -0500

    Can create a single user

--- src/psql_server/server.clj
+++ src/psql_server/server.clj
@@ -2,7 +2,7 @@
   (:require [mount.core :refer [defstate]]
             [ring.adapter.jetty :as ring]
             [environ.core :refer [env]]
-            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [ring.logger :refer [wrap-with-logger]]
             [psql-server.middleware :as middleware]
             [psql-server.routes :refer [routes]])
@@ -13,9 +13,11 @@
       (middleware/wrap-data)
       (middleware/wrap-content-type "application/json")
       (wrap-json-response)
-      (wrap-with-logger)))
+      (wrap-json-body {:keywords? true})
+      (wrap-with-logger)
+      (middleware/wrap-exceptions)))
 
-(defstate server 
+(defstate server
   :start (ring/run-jetty #'app {:port (Integer. (env :port))
                                 :join? false})
   :stop (.stop server))
