01a1b8da8891449bb65d2a6e054549242bdfca17
Adam <adam.cumiskey@gmail.com>
2018-03-01 00:33:42 -0500

    and fix test imports

--- test/psql_server/core_test.clj
+++ test/psql_server/core_test.clj
@@ -1,6 +1,6 @@
-(ns server.core-test
+(ns psql-server.core-test
   (:require [clojure.test :refer :all]
-            [server.core :refer :all]))
+            [psql-server.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
